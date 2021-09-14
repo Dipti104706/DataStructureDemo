977a6315dd660d2108f656f78eb8d47ceea437cf
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-14 22:02:36 +0530

    [Dipti] added user case 4 for inserting elements at the middle in linked list

--- DataStructureDemo/CustomLinkedList.cs
+++ DataStructureDemo/CustomLinkedList.cs
@@ -42,7 +42,7 @@ namespace DataStructureDemo
         }
 
        //method for inserting element at front
-       public void InsertFront(int new_data)
+        public void InsertFront(int new_data)
         {
             //We will create a new node.The next of the node will point to the head of the linked list
             Node new_node = new Node(new_data);
@@ -54,11 +54,35 @@ namespace DataStructureDemo
         }
 
        //Method for Appending elements at last in the list
-       public void Append(int new_data)
+        public void Append(int new_data)
         {
             InsertLast(new_data);
         }
 
+       //method for Insert at middle of a linked list
+        public void InsertMid(int new_data)
+        {
+            Node new_node = new Node(new_data);
+            Node pos = head;
+            int len = 0;
+            //calculate the length of linked list
+            while(pos !=null)
+            {
+                len++;
+                pos = pos.next;
+            }
+            //finding middle possition
+            int count = ((len % 2) == 0) ? (len / 2) : (len + 1) / 2;
+            pos = head;
+            //pos is the pointer to the node after which the new node to be insert
+            while(count-- >1)
+            {
+                pos = pos.next;
+            }
+            new_node.next = pos.next;
+            pos.next = new_node;
+        }
+       
        //method for displaying elements in linked list
        public void Display()
         {
